0c1e03cd8d5a439c756f14629ea631997e4b6e4f
hahakumquat <michael.zh.ge@gmail.com>
2015-06-23 19:31:40 -0400

    Updates

--- 22/22.py
+++ 22/22.py
@@ -13,9 +13,9 @@ obtain a score of 938 x 53 = 49714.
 What is the total of all the name scores in the file?
 '''
 import string
-
+import time
 #Variables
-
+t = time.time()
 #File Input
 with open("names.txt") as f:
     names = f.read().split(",")
@@ -34,4 +34,7 @@ for letter in a:
     for name in letter:
         ctr += 1
         tot += ctr * (sum(map(ord, name[1:len(name)-1])) - 64*(len(name) - 2))
+
+t2 = time.time()
 print tot
+print t2 -t
